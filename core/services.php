f604e3bbb67b2ebc5833f6ed6834c319cd730322
Ricki Hastings <ricki2k7@hotmail.co.uk>
2011-06-08 16:51:44 +0100

    updated file headers

--- core/services.php
+++ core/services.php
@@ -4,8 +4,8 @@
 * Acora IRC Services
 * core/services.php: Collection of functions for basic usage
 * 
-* Copyright (c) 2009 Acora (http://gamergrid.net/acorairc)
-* Coded by N0valyfe and Henry of GamerGrid: irc.gamergrid.net #acora
+* Copyright (c) 2009 Acora (http://cia.vc/stats/project/acorairc)
+* Written by Ricki, Henry, Shaun and help from others: irc.ircnode.org #acora
 *
 * This project is licensed under the GNU Public License
 *
